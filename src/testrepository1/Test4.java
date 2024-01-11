7f66f4f23baa42ede842f6adfd0a08a864522cd3
Gurpreet <gurpreet10us@gmail.com>
2024-01-11 13:46:21 -0500

    name in your native language

---
+++ src/testrepository1/Test4.java
@@ -0,0 +1,14 @@
+package testrepository1;
+
+public class Test4 {
+public static void main(String[] args) {
+	char ch = '\u0A17';
+	char ch1 = '\u0A41';
+	char ch2 = '\u0A30';
+	char ch3 = '\u0A2A';
+	char ch4 = '\u0A51';
+	char ch5 = '\u0A40';
+	char ch6 = '\u0A24';
+	System.out.println(ch+""+ch1+""+ch2+""+ch3+""+ch4+""+ch5+""+ch6);
+}
+}
