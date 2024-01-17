2edce45023cdedba902e43b8ac106ea76120f7a4
Gurpreet <gurpreet10us@gmail.com>
2024-01-17 13:45:28 -0500

    Write a Java program to convert seconds to hour, minute and seconds

---
+++ src/testrepository1/Test6.java
@@ -0,0 +1,15 @@
+package testrepository1;
+
+public class Test6 {
+public static void main(String[] args) {
+	int i = 86399/24;
+	int	j= 86399%24;
+	int k = i%60;
+	int l = i/60;
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+
+	
+}
+}
