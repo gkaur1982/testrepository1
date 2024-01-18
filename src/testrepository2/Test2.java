8235dff20ee2bad7fbc19dea8bc693e0a1fa1723
Gurpreet <gurpreet10us@gmail.com>
2024-01-18 09:28:36 -0500

    Write a Java program that reads a number and display the square, cube, and fourth power

---
+++ src/testrepository2/Test2.java
@@ -0,0 +1,15 @@
+package testrepository2;
+
+class Test2{
+	public static void main(String[] args) {
+	int i = 2; 
+	double square = Math.pow(i, 2);
+	double cube = Math.pow(i, 3);
+	double power = Math.pow(i, 4);
+	//int q = Math.min(i, j);
+	System.out.println("Square:"+square);
+	System.out.println("Cube:"+cube);
+	System.out.println("Fourth power:"+power);
+
+	}
+}
