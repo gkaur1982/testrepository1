eb617276e0a2b8343ddef7c2f1eb1396d9004be9
Gurpreet <gurpreet10us@gmail.com>
2024-01-18 09:12:51 -0500

    Sum, Difference, Product, Average, Distance, Maximum and Minimum

--- src/testrepository2/Test1.java
+++ src/testrepository2/Test1.java
@@ -3,9 +3,20 @@ package testrepository2;
 class Test1{
 	public static void main(String[] args) {
 	int i = 10; 
-	int j = i++ + ++i; 
-	System.out.println(i);
-	System.out.println(j);
-
+	int j = 6;
+	int k= i+j;
+	int l= i-j;
+	int m=i*j;
+	int n = (i+j)/2;
+	int o = i-j;
+	int p = Math.max(i, j);
+	int q = Math.min(i, j);
+	System.out.println("Sum:"+k);
+	System.out.println("Minus:"+l);
+	System.out.println("Product:"+m);
+	System.out.println("Average:"+n);
+	System.out.println("Distance:"+o);
+	System.out.println("Max:"+p);
+	System.out.println("Min:"+q);
 	}
 }
