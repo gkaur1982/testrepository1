c3a3c8dc1aae1aea985b3c32fe85a96d2b92bcc8
Gurpreet <gurpreet10us@gmail.com>
2024-05-22 14:06:28 -0400

    Objects

---
+++ src/testrepository1/Objects.java
@@ -0,0 +1,35 @@
+package testrepository1;
+
+public class Objects {
+public static void main(String[] args) {
+	
+}
+}
+class Human {
+	String name;
+	int age;
+	double weight;
+	int height;
+	String country;
+}
+class Mobile {
+	String Company;
+	int cost;
+	double version;
+	String color;
+	int memory;
+}
+class Desktop {
+	String Company;
+	int cost;
+	double version;
+	String color;
+	int memory;
+}
+class College {
+	String name;
+	int buildin;
+	String University;
+	String courses;
+	double Noofstudent;
+}
