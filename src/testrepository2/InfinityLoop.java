2ba290f72a2a4998c869227ebb02587ce4a348f8
Gurpreet <gurpreet10us@gmail.com>
2024-05-22 10:03:55 -0400

    Infinty Loop

---
+++ src/testrepository2/InfinityLoop.java
@@ -0,0 +1,10 @@
+package testrepository2;
+
+public class InfinityLoop {
+	public static void main(String[] args) {
+        for (;;) {
+            System.out.println("This is an infinite loop using for(;;)");
+           
+        }
+    }
+}
